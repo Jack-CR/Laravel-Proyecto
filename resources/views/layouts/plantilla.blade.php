57ab904093e4b552dfe7088a6c462ebaf2ef29ee
Yacdany <jack20-21@hotmail.com>
2021-05-28 20:39:13 -0600

    Comienzo de Proyecto

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('bootstrap-5.0/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div class="header">
+        @yield('header')
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="{{route('inicio.index')}}">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{route('inicio.index')}}">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                   <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li> 
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+              @auth
+               
+                 {{--  <a href="{{route('dashboard')}}"><label for="">{{Auth::user()->name}}</label></a> --}}
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      {{Auth::user()->name}}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="{{route('profile.show')}}">Perfil</a></li>
+                      <li><a class="dropdown-item" href="{{route('dashboard')}}">Mis Productos</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#"></a>
+                        <form action="{{route('logout')}}" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-primary m-3">Cerrar Sessión</button>
+                        </form>
+                      </li>
+                    </ul>
+                  </li> 
+
+                 {{--  <form action="{{route('logout')}}" method="post">
+                    @csrf
+                    
+                     <button type="submit" class="btn btn-primary">Cerrar Sessión</button>
+                  </form> --}}
+                
+                
+                 
+                @else
+
+                  <div class="p-3">
+                  <a name="" id="" class="btn btn-primary " href="{{route('login')}}" role="button">Login</a>
+                  <a name="" id="" class="btn btn-primary p-" href="{{route('register')}}" role="button">Registrarse</a>
+                </div>
+
+              @endauth
+              
+            </div>
+          </nav>
+    </div>
+
+    <div class="container">
+        @yield('container')
+    </div>
+
+    <div class="footer">
+        @yield('footer')
+    </div>
+
+    <script src="{{asset('bootstrap-5.0/js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.plantilla')
+
+@section('title','Inicio')
+
+
+@section('header')
+@endsection
+
+@section('container')
+
+
+@endsection
